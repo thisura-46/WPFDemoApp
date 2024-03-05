517563c44cd52a634ffe481e25345367980711f2
Thisura Ramanayake <thisura.ramanayake@ukg.com>
2024-03-05 14:07:39 +0530

    Built the User Interface of the demo App

--- WiredBrainCoffee.CustomerApp/MainWindow.xaml.cs
+++ WiredBrainCoffee.CustomerApp/MainWindow.xaml.cs
@@ -23,7 +23,20 @@ namespace WiredBrainCoffee.CustomerApp
 
         private void ButtonAddCustomer_Click(object sender, RoutedEventArgs e)
         {
-            btnAddCustomer.Content = "Customer Added";
+            //btnAddCustomer.Content = "Customer Added";
+        }
+
+        private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
+        {
+            //var column = (int)CustomerListGrid.GetValue(Grid.ColumnProperty);
+            //var newColumn = column == 0 ? 2 : 0;
+
+            //CustomerListGrid.SetValue(Grid.ColumnProperty, newColumn);
+
+            var column = Grid.GetColumn(CustomerListGrid);
+            var newColumn = column == 0 ? 2 : 0;
+
+            Grid.SetColumn(CustomerListGrid, newColumn);
         }
     }
 }
